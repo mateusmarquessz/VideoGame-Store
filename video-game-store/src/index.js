beab11fbef3930eff336ee59b53bba5656b81399
MateusMarquessz <mateusmarques53@outlook.com>
2023-11-23 19:06:35 -0300

    arrumando o index

--- video-game-store/src/index.js
+++ video-game-store/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './componetes/index.css';
-import App from './App';
+import NavbarResp from './NavbarResp';
+import ImageCarousel from './GaleriaRotativa'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavbarResp/>
+    <ImageCarousel/>
   </React.StrictMode>
 );
