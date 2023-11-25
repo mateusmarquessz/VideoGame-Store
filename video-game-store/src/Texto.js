58302525c14cc5480d4908e73c9ecd0cfe4658cc
MateusMarquessz <mateusmarques53@outlook.com>
2023-11-25 03:28:54 -0300

    Texto do lado da Galeria

---
+++ video-game-store/src/Texto.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './componetes/Texto.css';
+
+function Texto() {
+  return (
+    <div className="divTexto">
+      <h1>Up to 98% OFF</h1>
+      <p>The largest collection of premium gaming titles and hardware</p>
+      <h2>Get Deals</h2>
+    </div>
+  );
+}
+
+export default Texto;
