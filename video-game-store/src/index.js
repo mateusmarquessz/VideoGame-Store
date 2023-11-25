f3a6fe420497e981cdc85c6c713bfa8269e430b6
MateusMarquessz <mateusmarques53@outlook.com>
2023-11-25 03:28:37 -0300

    Add elementos

--- video-game-store/src/index.js
+++ video-game-store/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './componetes/index.css';
 import NavbarResp from './NavbarResp';
 import ImageCarousel from './GaleriaRotativa'
+import Informacoes from './Infor'
+import Game from './Games';
+import Texto from './Texto'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NavbarResp/>
+    <Texto/>
     <ImageCarousel/>
+    <Informacoes/>
   </React.StrictMode>
 );
