1a77d7a4dc50436c41dcfaca42e0d79aa66f3595
MateusMarquessz <mateusmarques53@outlook.com>
2023-11-25 03:28:27 -0300

    Informacoes embaixo da Galeria

---
+++ video-game-store/src/Infor.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './componetes/Infor.css';
+// Componente de Seção
+const Informacoes = () => {
+  return (
+    <div class="parent">
+      <div class="div1"> 
+      <a>
+        The Best Price      
+      </a>
+      </div>
+      <div class="div2"> 
+      <a>
+        Secure Payments
+      </a>
+      </div>
+      <div class="div3">
+        <a>
+        Join 1M + Members
+        </a> 
+        </div>
+    </div>
+    );
+}
+
+export default Informacoes;
