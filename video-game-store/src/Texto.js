1afa8dcfaf451a6950e84a47e13bde4f44a48757
MateusMarquessz <mateusmarques53@outlook.com>
2023-12-04 14:47:25 -0300

    Ajeitando elementos

--- video-game-store/src/Texto.js
+++ video-game-store/src/Texto.js
@@ -4,7 +4,9 @@ import './componetes/Texto.css';
 function Texto() {
   return (
     <div className="divTexto">
-      <h1>Up to 98% OFF</h1>
+      <h1>
+      Up to <span className="redText">98%</span> OFF
+      </h1>
       <p>The largest collection of premium gaming titles and hardware</p>
       <h2>Get Deals</h2>
     </div>
