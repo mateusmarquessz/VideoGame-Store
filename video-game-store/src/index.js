e5163df73338bd0e48b6fd00ee9863b332518dc3
MateusMarquessz <mateusmarques53@outlook.com>
2023-12-04 15:02:51 -0300

    atualizacao do css

--- video-game-store/src/index.js
+++ video-game-store/src/index.js
@@ -11,8 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NavbarResp/>
+    <div className='DivCentral'>
     <Texto/>
     <ImageCarousel/>
     <Informacoes/>
+    </div>
   </React.StrictMode>
 );
